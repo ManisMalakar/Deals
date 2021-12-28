6979dbecdec89005b62db63b81d60f6ecd151766
Manish Mali <mani.malakar12@gmail.com>
2021-12-28 11:38:25 -0500

    First Edit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FindDeals from './components/FindDeals';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <FindDeals></FindDeals>
+      </header>
+    </div>
+  );
+}
+
+export default App;
